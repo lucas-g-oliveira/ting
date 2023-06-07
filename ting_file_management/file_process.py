c6f8133a160c524d21f43ca0cc1e5cc37779b532
Lucas G <lucasg.devmail93@gmail.com>
2023-06-07 12:01:01 -0300

    feat: implemented function process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,21 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
+from ting_file_management.file_management import txt_importer
+import sys
+
+
+def process(path_file, instance: Queue):
+    for i in range(len(instance)):
+        if path_file == instance.search(i)["nome_do_arquivo"]:
+            return instance.search(i)
+
+    data = txt_importer(path_file)
+    instance.enqueue({
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(data),
+        "linhas_do_arquivo": data,
+    })
+    print(instance.search(len(instance)-1), file=sys.stdout)
+    return
 
 
 def remove(instance):
