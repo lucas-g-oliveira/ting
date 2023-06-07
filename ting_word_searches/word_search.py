04ee826c5084d3eede06365780df513c27fc21b3
Lucas G <lucasg.devmail93@gmail.com>
2023-06-07 18:49:07 -0300

    feat: implemented function exists_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,27 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
 
 
-def search_by_word(word, instance):
+def exists_word(word, instance: Queue):
+    result = []
+    for file in range(len(instance)):
+        ocorrencias = []
+        lines = instance.search(file)["linhas_do_arquivo"]
+
+        for line_index in range(len(lines)):
+            if str(word).lower() in str(lines[line_index]).lower():
+                ocorrencias.append({"linha": line_index + 1})
+
+        if len(ocorrencias):
+            result.append(
+                {
+                    "palavra": word,
+                    "arquivo": instance.search(file)["nome_do_arquivo"],
+                    "ocorrencias": ocorrencias,
+                }
+            )
+
+    return result
+
+
+def search_by_word(word, instance: Queue):
     """Aqui irá sua implementação"""
